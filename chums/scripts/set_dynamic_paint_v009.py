f6e6c656d4296f12ec4580ed2e98d28719359bee
Conrad Dueck <conrad.dueck@gmail.com>
2023-06-21 23:39:53 -0500

    latest version used

--- chums/scripts/set_dynamic_paint_v009.py
+++ chums/scripts/set_dynamic_paint_v009.py
@@ -3,16 +3,18 @@ import os
 
 print('\n\nHELLO')
 brushes = ['chr_flieswitheagles_scene_ma:legs','chr_luna_scene_ma:foot_R','chr_luna_scene_ma:foot_L','chr_ira_scene_ma:body']
-canvas = bpy.data.objects['env.loonbeach:ground.002']
-this_pass = "dynamic_paint_rim_"
-brush_displace = 1.0
+canvas = bpy.data.objects['env.loonbeach:beach_canvas']
+this_pass = "dynamic_paint_core_"
+brush_displace = 0.25
 canvas_displace = 0.01
 #shots = {'020':10,'030':10,'050':10}
 shots = {'010':146,'020':198,'030':64,'050':124,'070':280,'080':262,'090':43,'110':73,'120':172}
 paintpath = ''
-last_shot = ''
 viewlayer = bpy.context.view_layer.name
-shot_end, cache_offset, render_end = 0
+shot_end = 0
+cache_offset = 0
+render_end = 0
+resolution = 4096
 #define brush objects
 brushes = ['chr_flieswitheagles_scene_ma:legs','chr_luna_scene_ma:foot_R','chr_luna_scene_ma:foot_L','chr_ira_scene_ma:body']
 #collect all the mayaCache collections in the file
@@ -36,10 +38,8 @@ def set_collection_excluded(scene, view_layer_name, collection_name, exclude):
 
 def dp_brush_setup(_brushes,_cache,offset):
     set_collection_excluded(bpy.context.scene, viewlayer, cache.name, False)
-    print("\n\n", shot, cache.name, shot_end, cache_offset)
     #setup BRUSH es
     for brush in _brushes:
-        print('brush: ', brush)
         for obj in _cache.objects:
             if obj.type == 'MESH':
                 obj.scale = (0.0,0.0,0.0)
@@ -49,12 +49,12 @@ def dp_brush_setup(_brushes,_cache,offset):
             if brush in obj.name and obj.name in bpy.context.view_layer.objects:
                 bpy.context.scene.frame_current += 1
                 brush_obj = obj
+                print('Setup brush: ', brush)
                 print('brush_obj: ',brush_obj)
                 brush_obj.select_set(True)
                 bpy.context.view_layer.objects.active = brush_obj
                 #offset mesh cache
                 for mod in brush_obj.modifiers:
-                    print("mod:", mod.name)
                     if mod.type == 'MESH_SEQUENCE_CACHE':
                         mod.cache_file.frame_offset = offset
                         print("OFFSET ", brush_obj.name, " BY ", offset ," TO ", mod.cache_file.frame_offset)
@@ -96,16 +96,24 @@ for shot in shots.keys():
     #find mayaCache for current shot
     for cache in caches:
         if ("_sh"+shot+"_") in cache.name:
+            this_offset = 0
+            for shot_key in shots.keys():
+                print("shot_key", shot_key)
+                if int(shot_key) < int(shot):
+                    this_offset = this_offset + shots[shot_key] + 1
+                    print("this_offset: ", this_offset)
+            cache_offset = this_offset
+            print("shot", shot)
+            print("cache", cache)
+            print("cache_offset", cache_offset)
             dp_brush_setup(brushes,cache,cache_offset)
     #deselect all objects
     for ob in bpy.data.objects:
         ob.select_set(False)
     render_end += (shot_end + 1)
-    if last_shot == '':
-        last_shot = shot
-        cache_offset = shots[last_shot]
-    else:
-        cache_offset += shots[last_shot]
+    
+    
+    
 
 #setup CANVAS
 canvas.select_set(True)
@@ -114,11 +122,11 @@ print('canvas: ', canvas.name)
 
 #set output paintpath for image sequence output
 this_version = ''
-for i in os.path.basename(bpy.data.filepath).split("_"):
+for i in os.path.basename(bpy.data.filepath).replace(".blend","").split("_"):
     if i.startswith("v") and i[-1] in "0123456789":
         this_version += i
 print(this_version)
-paintpath = os.path.join(os.path.dirname(bpy.data.filepath), (this_pass+this_version), "all")
+paintpath = os.path.join(os.path.dirname(bpy.data.filepath), (this_pass+this_version))
 print('paintpath: ', paintpath)
 
 #handle output path existence
@@ -132,10 +140,10 @@ if not('DP Displace' in canvas.modifiers):
     dp_cv_displace.strength = canvas_displace
     dp_cv_displace.mid_level = 0.0
 #dynamic paint canvas modifier
-if not('Dynamic Paint New' in canvas.modifiers):
-    dp_canvas_mod = canvas.modifiers.new(name='Dynamic Paint New',type='DYNAMIC_PAINT')
+if not('Dynamic Paint Canvas' in canvas.modifiers):
+    dp_canvas_mod = canvas.modifiers.new(name='Dynamic Paint Canvas',type='DYNAMIC_PAINT')
 else:
-    dp_canvas_mod = canvas.modifiers['Dynamic Paint New']
+    dp_canvas_mod = canvas.modifiers['Dynamic Paint Canvas']
 print('CANVAS bpy.context.view_layer.objects.active:', bpy.context.view_layer.objects.active)
 dp_canvas_mod.ui_type = 'CANVAS'
 #initialize canvas if none found
@@ -144,12 +152,12 @@ if dp_canvas_mod.canvas_settings == None:
 dp_canvas_mod.canvas_settings.canvas_surfaces[0].frame_start = 1
 dp_canvas_mod.canvas_settings.canvas_surfaces[0].frame_end = render_end
 dp_canvas_mod.canvas_settings.canvas_surfaces[0].surface_format = 'IMAGE'
-dp_canvas_mod.canvas_settings.canvas_surfaces[0].image_resolution = 2048
+dp_canvas_mod.canvas_settings.canvas_surfaces[0].image_resolution = resolution
 dp_canvas_mod.canvas_settings.canvas_surfaces[0].use_antialiasing = True
 dp_canvas_mod.canvas_settings.canvas_surfaces[0].image_output_path = paintpath
 dp_canvas_mod.canvas_settings.canvas_surfaces[0].uv_layer = 'UVMap'
 dp_canvas_mod.canvas_settings.canvas_surfaces[0].use_output_a = True
-dp_canvas_mod.canvas_settings.canvas_surfaces[0].output_name_a = (cache.name.replace('_mayaCache','')[:-6] + '_paintmap_base_')
+dp_canvas_mod.canvas_settings.canvas_surfaces[0].output_name_a = (cache.name.replace('_mayaCache','')[:-6] + '_' + this_pass)
 dp_canvas_mod.canvas_settings.canvas_surfaces[0].brush_collection = bpy.data.collections['sh000_mayaCaches']
 #dp_canvas_mod.canvas_settings.canvas_surfaces[0].use_output_b = True
 #dp_canvas_mod.canvas_settings.canvas_surfaces[0].output_name_b = (cache.name.replace('_mayaCache','')[:-6] + '_wetmap_base_')
@@ -167,8 +175,24 @@ if mybake:
     bpy.context.view_layer.objects.active = None
     print("COMPLETED: ", cache.name)
     
-#for o in bpy.data.objects:
-#    if len(o.modifiers) > 0 and o.modifiers[0].type == 'MESH_SEQUENCE_CACHE' and not(o.modifiers[0].cache_file.frame_offset == 0.0):
-#        _offset = o.modifiers[0].cache_file.frame_offset
-#        o.modifiers[0].cache_file.frame_offset = 0
-#        print("OFFSET ", o.name, " FROM ", _offset, " TO ", o.modifiers[0].cache_file.frame_offset)
+'''
+# CLEAN UP CODE
+# COPY TO A BLANK SCRIPT WINDOW AND EXECUTE BEFORE RUNNING DYNAPAINT AGAIN
+import bpy
+for o in bpy.data.objects:
+    if len(o.modifiers) > 0:
+        if o.modifiers[0].type == 'MESH_SEQUENCE_CACHE' and not(o.modifiers[0].cache_file.frame_offset == 0.0):
+            _offset = o.modifiers[0].cache_file.frame_offset
+            o.modifiers[0].cache_file.frame_offset = 0
+        if 'DP Displace' in o.modifiers:
+            o.modifiers.remove(o.modifiers['DP Displace'])
+        if 'Dynamic Paint Canvas' in o.modifiers:
+            o.modifiers.remove(o.modifiers['Dynamic Paint Canvas'])
+        if 'Dynamic Paint Brush' in o.modifiers:
+            o.modifiers.remove(o.modifiers['Dynamic Paint Brush'])
+    if o.animation_data is not None and o.animation_data.action is not None:
+        for fc in o.animation_data.action.fcurves:
+            if fc.data_path == 'scale':
+                o.animation_data.action.fcurves.remove(fc)
+        o.scale = (1.0,1.0,1.0)
+'''
