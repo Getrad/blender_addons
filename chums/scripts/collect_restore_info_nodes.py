efa88d3a5f42f9c06881f1db3b20e2c759835dc0
Conrad Dueck <Getrad@users.noreply.github.com>
2023-12-20 00:18:01 -0600

    updates

--- chums/scripts/collect_restore_info_nodes.py
+++ chums/scripts/collect_restore_info_nodes.py
@@ -1,24 +1,34 @@
 import bpy
 
+print("\n\nSTART")
+
 def gn_gather_deps(gntree):
     gn_deps = []
     for node in gntree.nodes:
-        if node.type in ['OBJECT_INFO','COLLECTION_INFO']:
-            print(node.name, node.label, node.inputs[0].default_value.name)
-            gn_deps.append((node.name, node.inputs[0].default_value.name))
-            node.label = node.inputs[0].default_value.name
-            node.inputs[0].default_value = None
+        if node.type in ['OBJECT_INFO','COLLECTION_INFO','STORE_NAMED_ATTRIBUTE']:
+            print(node.name)
+            #print('\t',node.label, node.inputs[0].default_value.name)
+            if node.type == 'STORE_NAMED_ATTRIBUTE':
+                gn_deps.append((node.name, node.inputs[2].default_value))
+                node.label = node.inputs[2].default_value
+                node.inputs[2].default_value = ''
+            else:
+                gn_deps.append((node.name, node.inputs[0].default_value.name))
+                node.label = node.inputs[0].default_value.name
+                node.inputs[0].default_value = None
     return gn_deps
 
 def gn_restore_deps(gntree):
     gn_deps = []
     for node in gntree.nodes:
-        if node.type in ['OBJECT_INFO','COLLECTION_INFO']:
-            print(node.name, node.label, node.inputs[0].default_value)
+        if node.type in ['OBJECT_INFO','COLLECTION_INFO','STORE_NAMED_ATTRIBUTE']:
+            #print(node.name, node.label, node.inputs[0].default_value)
             if node.type == 'OBJECT_INFO':
                 node.inputs[0].default_value = bpy.data.objects[node.label]
-            else:
+            if node.type == 'COLLECTION_INFO':
                 node.inputs[0].default_value = bpy.data.collections[node.label]
+            if node.type == 'STORE_NAMED_ATTRIBUTE':
+                node.inputs[2].default_value = node.label
     return 0
 
 
