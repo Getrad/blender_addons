303d0671346c1e84b749a34649aabda4ec9c6428
Conrad Dueck <Getrad@users.noreply.github.com>
2024-01-14 13:51:12 -0600

    added label update function

--- chums/scripts/geonodes_xfer_w_labels_008.py
+++ chums/scripts/geonodes_xfer_w_labels_008.py
@@ -1,10 +1,44 @@
 import bpy
 
-gather_restore = 0  #gather(0) restore(1)
+gather_restore = 1  #gather(0) restore(1) update(2)
 
 print("\n\nSTART")
 # node_types = ['OBJECT_INFO','COLLECTION_INFO','STORE_NAMED_ATTRIBUTE','INPUT_ATTRIBUTE']
 node_types = ['OBJECT_INFO','COLLECTION_INFO','INPUT_ATTRIBUTE']
+
+def gn_update_labels(gntree):
+    gn_deps = []
+    colls = []
+    objs = []
+    for node in gntree.nodes:
+        if node.type in node_types:
+            if node.type == 'STORE_NAMED_ATTRIBUTE':
+                print("\t", node.name, node.type)
+                if (len(node.inputs[2].default_value) > 1):
+                    print('\t',node.label, node.inputs[2].identifier, node.inputs[2].default_value)
+                    gn_deps.append((node.name, node.inputs[2].default_value))
+                    node.label = node.inputs[2].default_value
+                    print("UPDATE:\t", node.name, " \tto\t", node.inputs[2].default_value)
+            elif node.type == 'INPUT_ATTRIBUTE':
+                if (len(node.inputs[0].default_value) > 1):
+                    print('\t',node.label, node.inputs[0].identifier, node.inputs[0].default_value)
+                    gn_deps.append((node.name, node.inputs[0].default_value))
+                    node.label = str(node.inputs[0].default_value)
+                    print("UPDATE:\t", node.name, " \tto\t", node.inputs[0].default_value)
+            elif (node.inputs[0].default_value is not None):
+                gn_deps.append((node.name, node.inputs[0].default_value.name))
+                node.label = node.inputs[0].default_value.name
+                if node.type == node_types[1]:
+                    colls.append(node.label)
+                elif node.type == node_types[0]:
+                    objs.append(node.label)
+                print("UPDATE:\t", node.name, " \tto\t", node.inputs[0].default_value.name)
+            elif node.type == 'GROUP' and node.node_tree is not None:
+                gn_gather_deps(node.node_tree)
+            else:
+                print('\t',node.name, ' not managed - skipping')
+    return (colls, objs)
+
 
 def gn_gather_deps(gntree):
     gn_deps = []
@@ -30,7 +64,7 @@ def gn_gather_deps(gntree):
                 node.label = node.inputs[0].default_value.name
                 if node.type == node_types[1]:
                     colls.append(node.label)
-                elif node.type == node_types[0:
+                elif node.type == node_types[0]:
                     objs.append(node.label)
                 node.inputs[0].default_value = None
             elif node.type == 'GROUP' and node.node_tree is not None:
@@ -49,16 +83,12 @@ def gn_restore_deps(gntree):
                 try:
                     node.inputs[0].default_value = bpy.data.objects[node.label]
                 except:
-                    #print("Failed to set:", gntree.name, node.name, node.inputs[0].identifier, "to:", node.label)
                     objs.append(node.label)
-                    #print("OBJ:\t",node.label)
             elif node.type == 'COLLECTION_INFO':
                 try:
                     node.inputs[0].default_value = bpy.data.collections[node.label]
                 except:
-                    #print("Failed to set:", gntree.name, node.name, "to:", node.label)
                     colls.append(node.label)
-                    #print("COLL:\t", node.label)
             elif node.type == 'STORE_NAMED_ATTRIBUTE':
                 node.inputs[2].default_value = node.label
             elif node.type == 'INPUT_ATTRIBUTE':
@@ -75,16 +105,19 @@ for ob in bpy.context.selected_objects:
     for mod in ob.modifiers:
         if mod.type == 'NODES':
             print(mod.name, mod.node_group)
-            #if mod.node_group.users > 1:
-            #    mod.node_group = mod.node_group.copy()
             if gather_restore == 0:
                 break_dependency = gn_gather_deps(mod.node_group)
                 mycolls.extend(break_dependency[0])
                 myobjs.extend(break_dependency[1])
-            else:
+            elif gather_restore == 1:
                 restore_dependency = gn_restore_deps(mod.node_group)
                 mycolls.extend(restore_dependency[0])
                 myobjs.extend(restore_dependency[1])
+            else:
+                update_dependency = gn_update_labels(mod.node_group)
+                mycolls.extend(update_dependency[0])
+                myobjs.extend(update_dependency[1])
+                
     print(ob.name)
     print("\nmycolls:")
     for a in mycolls: print(a)
