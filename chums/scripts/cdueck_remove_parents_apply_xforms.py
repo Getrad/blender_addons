a0a3cc73900e611876f9b72be3624a7e39fdcdd4
Conrad Dueck <conrad.dueck@gmail.com>
2025-01-27 20:20:59 -0600

    apply xforms script

---
+++ chums/scripts/cdueck_remove_parents_apply_xforms.py
@@ -0,0 +1,31 @@
+#Remove parents and apply xforms
+
+import bpy
+from mathutils import *
+
+ob = bpy.context.selected_objects[0]
+
+
+def collectFamTree(ob,famTree):
+    while ob.parent is not None:
+        if not(ob.parent in famTree):
+            famTree.insert(0, ob.parent)
+        ob = ob.parent
+        collectFamTree(ob,famTree)
+    return famTree
+        
+famTree = []
+
+ft = collectFamTree(ob,famTree)
+ft = ft.append(ob)
+
+
+for o in famTree:
+    print(o.name)
+    wld = o.matrix_world
+    bpy.context.view_layer.objects.active = o
+    bpy.ops.object.transform_apply(location = True, scale = True, rotation = True)
+    o.parent = None
+    o.matrix_world = wld
+    bpy.ops.object.transform_apply(location = True, scale = True, rotation = True)
+    
